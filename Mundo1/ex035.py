e7ba94dbaace3852e81828a08c0f968b5dd6fb29
Killandio Dantas <killandio@keymail.dev>
2024-11-27 01:08:29 -0300

    Last update: Mundo 1 - Curso em Vídeo Python

--- Mundo1/ex035.py
+++ Mundo1/ex035.py
@@ -6,6 +6,6 @@ lado2 = float(input("Informe o comprimento do segundo lado: "))
 lado3 = float(input("Informe o comprimento do terceiro lado: "))
 
 if lado1 < lado2 + lado3 and lado2 < lado1 + lado3 and lado3 < lado1 + lado2:
-    print("Esses lados podem formar um triângulo!")
+    print("Sim, esses lados podem formar um triângulo!")
 else:
     print("Esses lados NÃO conseguem formar um triângulo!")
